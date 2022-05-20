5adfe02399100fb0a7b3b5c2958ef3cacfa5d8bd
joaovictor <jc7155004@gmail.com>
2022-05-20 09:36:29 -0300

    materialui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,61 @@
 import React from "react";
 
+import { createTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+
 import Routes from "./Routes";
+/* import Appbar from './components/Appbar/Appbar'
+import Sidebar from './components/Sidebar/Sidebar' */
+
+//base style
+/* import "./styles/_base.scss"; */
+
+//create material UI theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f2aa26",
+      dark: "#f09c01",
+    },
+    secondary: {
+      main: "#11cb5f",
+    },
+    text: {
+      primary: "#373585",
+      secondary: "#a4a6b3",
+    },
+    background: {
+      default: "#f7f8fc",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins, sans- serif",
+    fontWeightBold: 700,
+    fontWeightMedium: 600,
+    fontWeightRegular: 400,
+    htmlFontSize: 8,
+  },
+});
 
 function App() {
+  const [drawerState, setDrawerState] = React.useState(false);
+
+  //handle drawer state
+  const handleDrawerState = (state: boolean) => {
+    setDrawerState(state);
+  };
+
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {/* appbar component  */}
+        {/*   <Appbar onClick={handleDrawerState} drawerState={drawerState}/> */}
+
+        {/* Sidebar  */}
+        {/*     <Sidebar onClick={handleDrawerState} drawerState={drawerState}/> */}
+        <Routes />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,62 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import makeStore from "./redux/store";
+import { createTheme } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
 
-import App from "./App";
+import Routes from "./Routes";
+/* import Appbar from "./components/Appbar/Appbar";
+import Sidebar from "./components/Sidebar/Sidebar"; */
 
-const store = makeStore();
+//base style
+/* import "./styles/_base.scss"; */
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
-  </Provider>,
-  document.getElementById("root")
-);
+//create material UI theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f2aa26",
+      dark: "#f09c01",
+    },
+    secondary: {
+      main: "#11cb5f",
+    },
+    text: {
+      primary: "#373585",
+      secondary: "#a4a6b3",
+    },
+    background: {
+      default: "#f7f8fc",
+    },
+  },
+  typography: {
+    fontFamily: "Poppins, sans- serif",
+    fontWeightBold: 700,
+    fontWeightMedium: 600,
+    fontWeightRegular: 400,
+    htmlFontSize: 8,
+  },
+});
+
+function App() {
+  const [drawerState, setDrawerState] = React.useState(false);
+
+  //handle drawer state
+  const handleDrawerState = (state: boolean) => {
+    setDrawerState(state);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        {/* appbar component  */}
+        {/* <Appbar onClick={handleDrawerState} drawerState={drawerState} /> */}
+
+        {/* Sidebar  */}
+        {/* <Sidebar onClick={handleDrawerState} drawerState={drawerState} /> */}
+        <Routes />
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
